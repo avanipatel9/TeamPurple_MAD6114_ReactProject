8f4d76871df18de4731055fe74d69757a33a36c7
Avani Patel <60161097+avanipatel9@users.noreply.github.com>
2021-03-22 17:57:40 +0530

    modified

--- demo/index.js
+++ demo/index.js
@@ -5,26 +5,8 @@ import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>react-simple-snake</h1>
+    <h1>Team Purple MAD6114 React Project</h1>
     <div className="textWrapper">
-      <p className="subTitle">
-        <a
-          href="https://www.npmjs.com/package/react-simple-snake"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          NPM
-        </a>{" "}
-        -
-        <a
-          href="https://github.com/MaelDrapier/react-simple-snake"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {" "}
-          Github
-        </a>
-      </p>
       <p id="instructions">Use the arrow keys or W/A/S/D to play</p>
       <p className="subTitle">
         A simple snake game created with the
